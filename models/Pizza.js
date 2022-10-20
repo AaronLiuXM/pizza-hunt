294b4352017013bb6e8791f4b004249236ff6f44
Aaron Liu <aaron155658@gmail.com>
2022-10-19 21:10:54 -0400

    add comment-routes

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,25 +1,43 @@
 const { schema, model, Schema } = require("mongoose");
 
-const PizzaSchema = new Schema({
-  pizzaName: {
-    type: String,
+const PizzaSchema = new Schema(
+  {
+    pizzaName: {
+      type: String,
+    },
+    createdBy: {
+      type: String,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    size: {
+      type: String,
+      default: "Large",
+    },
+    toppings: [],
+    comments: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
   },
-  createBy: {
-    type: String,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  size: {
-    type: String,
-    default: "Large",
-  },
-  toppings: [],
-});
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
 
 // create the Pizza using the PizzaSchema
 const Pizza = model("Pizza", PizzaSchema);
+
+PizzaSchema.virtual("commentCount").get(function () {
+  return this.comments.length;
+});
 
 // export the Pizza model
 module.exports = Pizza;
